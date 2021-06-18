549445e1b5ac1895d078616c0e0bd16aca57fd09
Mtaes <Mtaes@users.noreply.github.com>
2021-06-18 16:38:32 +0200

    Modify experiment 1

--- src/experiments.py
+++ src/experiments.py
@@ -34,8 +34,8 @@ def experiment_1():
 
     trainer = get_trainer(
         max_epochs=15,
-        min_delta=.001,
-        patience=4,
+        min_delta=.0001,
+        patience=5,
         version=ID
     )
     trainer.fit(model, data_loaders['train'], data_loaders['validate'])
