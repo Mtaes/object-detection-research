d98e78745906e87284be67e65dff5149c280202b
Mtaes <Mtaes@users.noreply.github.com>
2021-07-02 13:37:15 +0200

    Add experiment 12

--- src/experiments.py
+++ src/experiments.py
@@ -352,6 +352,39 @@ def experiment_11():
     trainer.test(test_dataloaders=data_loaders['test'])
 
 
+def experiment_12():
+    ID = 12
+    seed_everything(SEED, workers=True)
+    splits = get_dataset_split(PATH_TO_BEESDATASET, 2)
+    data_loaders = get_dataloaders(
+        DatasetClass=BeesDataset,
+        path_to_dataset=PATH_TO_BEESDATASET,
+        splits=splits,
+        transforms={'train': Compose([ToTensor()]), 'validate': Compose([ToTensor()]), 'test': Compose([ToTensor()])},
+        batch_size={'train': 4, 'validate': 4, 'test': 4}
+    )
+
+    model = fasterrcnn_resnet50_fpn(pretrained=True)
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, 2)
+    def optimizer_fn(params, lr):
+        return SGD(params, lr=lr, momentum=.9, weight_decay=.0001)
+    def lr_scheduler_fn(optimizer):
+        return lr_scheduler.StepLR(optimizer, step_size=3)
+    model = ObjectDetector(model, .02, optimizer_fn, lr_scheduler_fn)
+
+    trainer = get_trainer(
+        max_epochs=15,
+        min_delta=.001,
+        patience=5,
+        version=ID,
+        auto_lr_find=True
+    )
+    trainer.tune(model, data_loaders['train'], data_loaders['validate'])
+    trainer.fit(model, data_loaders['train'], data_loaders['validate'])
+    trainer.test(test_dataloaders=data_loaders['test'])
+
+
 EXPERIMENTS_DICT = {
     '1': experiment_1,
     '2': experiment_2,
@@ -363,5 +396,6 @@ EXPERIMENTS_DICT = {
     '8': experiment_8,
     '9': experiment_9,
     '10': experiment_10,
-    '11': experiment_11
+    '11': experiment_11,
+    '12': experiment_12
 }
