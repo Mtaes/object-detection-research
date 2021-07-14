85b780e8825869f5529b57124f6532c37b0cde14
Mtaes <Mtaes@users.noreply.github.com>
2021-07-14 05:06:03 +0200

    Add experiment 14

--- src/experiments.py
+++ src/experiments.py
@@ -340,6 +340,35 @@ def experiment_13():
     study.optimize(objective, n_trials=None, timeout=8.8 * 60 * 60)
 
 
+def experiment_14():
+    ID = 14
+    seed_everything(SEED, workers=True)
+    def get_model(lr, optimizer_fn, lr_scheduler_fn, update_lr_scheduler):
+        model = fasterrcnn_resnet50_fpn(pretrained=True)
+        in_features = model.roi_heads.box_predictor.cls_score.in_features
+        model.roi_heads.box_predictor = FastRCNNPredictor(in_features, 2)
+        model = ObjectDetector(model, lr, optimizer_fn, lr_scheduler_fn, update_lr_scheduler)
+        return model
+    data_module = BeesDataModule(
+        split_id=2,
+        batch_size=4,
+        transforms={'train': get_horizontal_flip(), 'validate': get_resize_image(), 'test': get_resize_image()}
+    )
+    objective = get_SGD_objective_fn(
+        get_model_fn=get_model,
+        data_module=data_module,
+        SGD_momentum=(.5, 1., False),
+        SGD_weight_decay=(1e-7, 1e-1, True),
+        RLROP_factor=(.3, .5, False),
+        learning_rate=(1e-4, 1e-1, True),
+        max_epochs=10,
+        limit_train_batches=.6,
+        SGD_nesterov=True
+    )
+    study = optuna.create_study(study_name=f'experiment_{ID}', storage=get_study_storage(), direction='maximize', pruner=optuna.pruners.MedianPruner(), load_if_exists=True)
+    study.optimize(objective, n_trials=None, timeout=8.8 * 60 * 60)
+
+
 EXPERIMENTS_DICT = {
     '1': experiment_1,
     '2': experiment_2,
@@ -354,4 +383,5 @@ EXPERIMENTS_DICT = {
     '11': experiment_11,
     '12': experiment_12,
     '13': experiment_13,
+    '14': experiment_14,
 }
